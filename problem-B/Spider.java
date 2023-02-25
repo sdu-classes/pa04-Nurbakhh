f2a91cfe4dd4053914a87f4c95f4b76f1b19e0ef
Nurbakhh <124429535+Nurbakhh@users.noreply.github.com>
2023-02-26 02:08:04 +0600

    Update Spider.java

---
+++ problem-B/Spider.java
@@ -0,0 +1,11 @@
+package css108.part4.B;
+
+public class Spider extends Animal{
+    public Spider(){
+        super(8);
+    }
+
+    public void eat(){
+        System.out.println("Spider eats");
+    }
+}
